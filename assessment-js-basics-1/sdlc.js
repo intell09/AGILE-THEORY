4e1acd94a4649a9e273cb6404bcbc66759628abd
inteldadeveloper <74120739+inteldadeveloper@users.noreply.github.com>
2022-08-26 21:44:17 -0400

    first commit

---
+++ assessment-js-basics-1/sdlc.js
@@ -0,0 +1,27 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+var Implementation = "This is the second step, in which the end goal of the";
+var Design = "design phase, lead developers and technical architects create the initial high-level design plan for the software and system. This includes the delivery of requirements used to create the Design Document Specification (DDS).";
+var Maintain = "n a post-production, live software environment, the system is in maintenance mode. No matter the number of users, the sophistication of the software and rigorous QA testing, issues will occur. ";
+var AnalysisOfRequirements = "The Software Development cycle is the begins with requirements analysis phase.";
+var TestingIntegration = "test the software against the requirements to make sure that the software is solving the needs addressed and outlined during the planning phase. ";
+var Planning = "Business requirements are compiled and analyzed by a business analyst, domain expert, and project manager.";
